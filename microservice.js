dbb0915e8ccf3525921777bbadceaba416172ded
oscardoudou <oscar9591@icloud.com>
2019-04-15 20:24:12 -0400

    set port env and hit the route

---
+++ microservice.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    marqdown  = require('./marqdown.js')
+var app = express();
+
+app.post('/markdown', 
+	function(req,res)
+	{
+		// console.log(req.body.markdown);
+		// //var text = marqdown.render( req.query.markdown );
+		// var text = marqdown.render( req.body.markdown );
+		res.send("Hello world");
+		// res.send( {preview: text} );
+	}
+);
+
+var port = process.env.APP_PORT;
+app.listen(port);
+console.log(`Listening on port ${port}...`);
