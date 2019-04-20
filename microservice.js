47944cdfe0768c864821f0b7bb810fb62a3b4097
oscardoudou <oscar9591@icloud.com>
2019-04-19 20:11:24 -0400

    [workaround]add bodyparse downgrade express to 3.x

--- microservice.js
+++ microservice.js
@@ -2,14 +2,23 @@ var express = require('express'),
     marqdown  = require('./marqdown.js')
 var app = express();
 
-app.get('/markdown', 
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.configure(function () {
+    app.use(express.logger('dev'));     / 'default', 'short', 'tiny', 'dev' /
+    app.use(express.bodyParser());
+});   
+
+app.post('/markdown', 
 	function(req,res)
 	{
 		// console.log(req.body.markdown);
-		// //var text = marqdown.render( req.query.markdown );
-		// var text = marqdown.render( req.body.markdown );
-		res.send("Hello world");
-		// res.send( {preview: text} );
+		// var text = marqdown.render( req.query.markdown );
+		console.log(req.body);
+		var text = marqdown.render( req.body.markdown );
+		// res.send("Hello world");
+		res.send( {preview: text} );
 	}
 );
 
